decd979db261d23994f5df3ac29426292168a3f2
Ravil <raviltchusmakaev@yandex.ru>
2023-01-18 12:10:42 +0300

    First Change

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
 
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.name = surname
+        self.courses_attached = []
+
+best_student = Student('Iron', 'man', '35')
+best_student.finished_courses += ['Git']
+best_student.courses_in_progress += ['Python']
+best_student.grades ['Git'] = [10, 10, 10, 10, 10]
+best_student.grades ['Python'] = [10, 10]
+
+print(best_student.finished_courses)
+print(best_student.courses_in_progress)
+print(best_student.grades)
+
+cool_mentor = Mentor('Bruce Wane', 'Spider Man')
+cool_mentor.courses_attached += ['Python']
+print(cool_mentor.courses_attached)
